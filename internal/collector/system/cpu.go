9851d570dddd94ad8a72774aaee348b3c639fa32
Aaron Mathis <aaron.mathis@gmail.com>
2025-04-01 16:48:13 -0500

    Fixed license headers on all .go files.

--- internal/collector/system/cpu.go
+++ internal/collector/system/cpu.go
@@ -16,7 +16,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with LeetScraper. If not, see https://www.gnu.org/licenses/.
+along with GoSight. If not, see https://www.gnu.org/licenses/.
 */
 
 // gosight/agent/internal/collector/system/cpu.go
