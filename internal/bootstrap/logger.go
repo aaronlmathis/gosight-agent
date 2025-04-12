420ebc631eb032bbdb7f55a9b89a81563b6654d1
Aaron Mathis <aaron.mathis@gmail.com>
2025-04-12 09:45:25 -0500

    Added multi-stage MFA. mfa.html template.. handle for /mfa.. helper functions for creating session and injecting context, clearing cookies, etc

--- internal/bootstrap/logger.go
+++ internal/bootstrap/logger.go
@@ -35,7 +35,7 @@ func SetupLogging(cfg *config.Config) {
 	if cfg.Agent.Environment == "dev" {
 		cfg.Logs.ErrorLogFile = ""
 	}
-	if err := utils.InitLogger(cfg.Agent.AppLogFile, cfg.Agent.ErrorLogFile, cfg.Logs.LogLevel); err != nil {
+	if err := utils.InitLogger(cfg.Agent.AppLogFile, cfg.Agent.ErrorLogFile, "access.log", cfg.Logs.LogLevel); err != nil {
 		fmt.Printf("Failed to initialize logger: %v\n", err)
 		os.Exit(1)
 	}
